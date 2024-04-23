f15bbda306f23052f5972bb428a3af970e0bf2a5
m_kermit <krasnopeeva@gmail.com>
2024-04-23 17:56:12 +0300

    KS-1-2 create new variable and print

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main(){
+    var numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+    var numberOfEmployees = 2000u
+    //println(numberOfEmployees)
+    numberOfEmployees = 1999u
+
+    println(numberOfEmployees)
+}
