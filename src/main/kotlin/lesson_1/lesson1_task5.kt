41d8a5ae629dade1a6a385f1ae672c882bdab7c5
m_kermit <krasnopeeva@gmail.com>
2024-04-24 21:37:17 +0300

    KS-1-5 create time constants and formatting

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,17 @@
 package org.example.lesson_1
 
-fun main(){
+/* Константы времени */
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
+
+fun main() {
+
     val secondsInSpaceTotal: Short = 6480
 
-    val hoursInSpace = secondsInSpaceTotal/ 3600
-    val minutesInSpace = (secondsInSpaceTotal % 3600)/ 60
-    val secondsInSpace = secondsInSpaceTotal % 60
+    val hoursInSpace = secondsInSpaceTotal / SECONDS_IN_HOUR
+    val minutesInSpace = (secondsInSpaceTotal % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val secondsInSpace = secondsInSpaceTotal % SECONDS_IN_MINUTE
 
     val formattedTime = String.format("%02d:%02d:%02d", hoursInSpace, minutesInSpace, secondsInSpace)
     print(formattedTime)
