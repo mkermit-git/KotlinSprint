55f697bcb10671324010648166db8d4c708ea2ca
m_kermit <krasnopeeva@gmail.com>
2024-04-24 22:16:14 +0300

    KS-2-5 formatted and commented

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,13 +1,16 @@
 package org.example.lesson_2
+
 import kotlin.math.pow
 
-fun main(){
+fun main() {
     val depositInitialAmount = 70000
-    val depositInterest = 16.7/100 //double для большей точности
+    val depositInterest = 16.7 / 100 //double для большей точности
     val depositTerm = 20
     val calculationPeriod = 1
+
+    // Расчет сложных процентов по формуле: A = P * (1 + i / n) ^ (n * t)
     val depositFinalAmount = depositInitialAmount *
-            (1 + depositInterest/calculationPeriod).pow(calculationPeriod * depositTerm)
+            (1 + depositInterest / calculationPeriod).pow(calculationPeriod * depositTerm)
 
     println(String.format("%.3f", depositFinalAmount))
 }
