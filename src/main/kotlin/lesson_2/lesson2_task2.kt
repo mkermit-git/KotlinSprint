b8bf4ceab320351aab88d1acff9f98fbc14b8f0a
m_kermit <krasnopeeva@gmail.com>
2024-04-24 21:57:30 +0300

    KS-2-2 formatted

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
-fun main(){
-    val numberOfPermStaff= 50
+fun main() {
+    val numberOfPermStaff = 50
     val salaryOfPermStaff = 30000
+
     // Расходы на выплату зарплаты постоянных сотрудников
     val payrollExpensePerm: Int = numberOfPermStaff * salaryOfPermStaff
     println(payrollExpensePerm)
 
-    val numberOfInternStaff= 30
+    val numberOfInternStaff = 30
     val salaryOfInternStaff = 20000
 
     // Общие расходы по ЗП после прихода стажеров
