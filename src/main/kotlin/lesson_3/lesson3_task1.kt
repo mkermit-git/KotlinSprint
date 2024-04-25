5b3e690083336ddd5d8ad4b7c49c1ebac95d562f
m_kermit <krasnopeeva@gmail.com>
2024-04-25 20:35:32 +0300

    KS-3-1 added more greetings

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -5,9 +5,11 @@ import java.time.LocalTime
 fun main() {
     val userName = "товарищ"
     val currentTime = LocalTime.now()
-    val greetingPhrase = when {
-        currentTime.hour < 16 -> "Добрый день"
-        else -> "Добрый вечер"
+    val greetingPhrase = when (currentTime.hour) {
+        in 6..11 -> "Доброе утро"
+        in 12..17 -> "Добрый день"
+        in 18..21 -> "Добрый вечер"
+        else -> "Доброй ночи"
     }
     println("$greetingPhrase, $userName!")
 }
