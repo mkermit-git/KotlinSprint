c95d23b2057589f6964868054b6fa1ae42a1bc68
m_kermit <krasnopeeva@gmail.com>
2024-04-30 12:44:28 +0300

    KS-2-5 monthly deposit interest and calc period

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -3,10 +3,10 @@ package org.example.lesson_2
 import kotlin.math.pow
 
 fun main() {
-    val depositInitialAmount = 70000
-    val depositInterest = 16.7 / 100 //double для большей точности
-    val depositTerm = 20
-    val calculationPeriod = 1
+    val depositInitialAmount = 50000
+    val depositInterest = (17.0 / 12) / 100 // Помесячная процентная ставка
+    val depositTerm = 2 // Срок вклада - 2 месяца
+    val calculationPeriod = 1 // Начисление процентов каждый месяц
 
     // Расчет сложных процентов по формуле: A = P * (1 + i / n) ^ (n * t)
     val depositFinalAmount = depositInitialAmount *
